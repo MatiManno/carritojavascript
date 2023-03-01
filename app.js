c895cc6a2f8b7b6ab591be678e1d51e03410419e
Matias Manno <mati.manno@gmail.com>
2023-03-01 19:31:09 -0300

    va de nuevo

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // Variables globales
 let productosCarrito = [];
 let total = 0;
+let productos = []; // Variable global para almacenar la información de los productos
 
 // Función para cargar los productos desde un archivo JSON usando fetch()
 function cargarProductos() {
   fetch('productos.json')
     .then(response => response.json())
-    .then(productos => {
+    .then(data => {
+      productos = data; // Guardar la información de los productos en la variable global
       // Crear las opciones para el select de productos
       const selectProducto = document.getElementById('producto');
       productos.forEach(producto => {
@@ -22,7 +24,7 @@ function cargarProductos() {
 function agregarProducto() {
   const producto = document.getElementById('producto').value;
   const cantidad = Number(document.getElementById('cantidad').value);
-  const productos = productosCarrito.filter(p => p.producto === producto);
+  const productoSeleccionado = productos.find(p => p.nombre === producto);
 
   // Validar que se ingresó una cantidad válida
   if (isNaN(cantidad) || cantidad <= 0) {
@@ -31,8 +33,7 @@ function agregarProducto() {
   }
 
   // Validar que hay suficientes existencias del producto
-  const stock = productos.length > 0 ? productos[0].stock : 0;
-  if (cantidad > stock) {
+  if (cantidad > productoSeleccionado.stock) {
     alert(`No hay suficientes existencias de ${producto}.`);
     return;
   }
@@ -48,7 +49,7 @@ function agregarProducto() {
   carrito.appendChild(li);
 
   // Calcular el total
-  const precio = productos[0].precio;
+  const precio = productoSeleccionado.precio;
   total += precio * cantidad;
   calcularTotal();
 }
@@ -64,34 +65,30 @@ function eliminarProducto(index) {
   carrito.removeChild(li);
 
   // Restar el precio del producto eliminado del total
-  const productos = productosCarrito.filter(p => p.producto === productoEliminado.producto);
-  const precio = productos[0].precio;
+  const productoSeleccionado = productos.find(p => p.nombre === productoEliminado.producto);
+  const precio = productoSeleccionado.precio;
   const cantidad = productoEliminado.cantidad;
   total -= precio * cantidad;
   calcularTotal();
 }
 
-// Función para calcular el total de la compra
+// Función para calcular el total del carrito
 function calcularTotal() {
   const totalElement = document.getElementById('total');
-  totalElement.innerHTML = `$${total.toFixed(2)}`;
-}
-
-// Función para vaciar el carrito
-function vaciarCarrito() {
-  // Vaciar el arreglo de productos
+  totalElement.innerHTML = `${total.toFixed(2)}`;
+  }
+  
+  // Función para vaciar el carrito
+  function vaciarCarrito() {
+  // Vaciar el arreglo de productos del carrito y resetear el total
   productosCarrito = [];
-
+  total = 0;
+  
   // Actualizar el HTML
   const carrito = document.getElementById('carrito');
-  while (carrito.firstChild) {
-    carrito.removeChild(carrito.firstChild);
-  }
-
-  // Reiniciar el total
-  total = 0;
+  carrito.innerHTML = '';
   calcularTotal();
-}
-
-// Cargar los productos al cargar la página
-document.addEventListener('DOMContentLoaded', cargarProductos);
+  }
+  
+  // Llamada a la función para cargar los productos al cargar la página
+  cargarProductos();
